22cc82c1a85b96975a7f7b4db1e76f5ca19f449a
jaskola <bartosz.michalowski.j@gmail.com>
2019-02-18 22:12:03 +0100

    Revert "Revert "Tests and documentation written, ready for merge""
    This reverts commit 69d138cd505c7cc4d848d5028ecbfe825d139f1b.

--- api/ann_test.go
+++ api/ann_test.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"testing"
 )
 
@@ -14,5 +18,59 @@ func TestCreateURL(t *testing.T) {
 	}
 	if outputHeaders != expectedHeaders {
 		t.Fatalf("Expected %s, got %s.", expectedHeaders, outputHeaders)
+	}
+}
+
+//Tests fetch function by comparing its output from querying temporary local server with predefined values
+func TestFetch(t *testing.T) {
+	file, err := os.Open("test_reply.json")
+	if err != nil {
+		t.Fatalf("Error opening file containing testing data: %v", err)
+	}
+	defer file.Close()
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Fatalf("Error reading file containing testing data: %v", err)
+	}
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s", content)
+	})
+
+	go http.ListenAndServe(":3000", nil)
+
+	response, err := fetch("http://localhost:3000/test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%s", response.FirstName)
+	compareResults(&response, t)
+}
+
+func compareResults(response *Result, t *testing.T) {
+	if response.FirstName != "FirstName" {
+		t.Errorf("Expected %s, got %s.", "FirstName", response.FirstName)
+	}
+	if response.SecondName != "SecondName" {
+		t.Errorf("Expected %s, got %s.", "SecondName", response.SecondName)
+	}
+	if response.Semester != "Semester" {
+		t.Errorf("Expected %s, got %s.", "Semester", response.Semester)
+	}
+	compareAnnoucements(response.Items, t)
+}
+
+func compareAnnoucements(annoucements []Annoucement, t *testing.T) {
+	if annoucements[0].Content != "Content" {
+		t.Errorf("Expected %s, got %s.", "Content", annoucements[0].Content)
+	}
+	if annoucements[0].Type != 1001 {
+		t.Errorf("Expected %d, got %d.", 1001, annoucements[0].Type)
+	}
+	if annoucements[1].Type != 1002 {
+		t.Errorf("Expected %d, got %d.", 1001, annoucements[1].Type)
+	}
+	if annoucements[1].Content != "" {
+		t.Errorf("Expected empty string, got %s", annoucements[1].Content)
 	}
 }
